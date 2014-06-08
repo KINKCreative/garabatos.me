aa119efd5eab4cf76547738edcd0bfb01f365d25
Klemen Novak <klemen@kinkcreative.com>
2014-06-08 08:22:18 -0700

    Updated cap config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,8 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@garabatos.me}
-role :web, %w{deploy@garabatos.me}
-role :db,  %w{deploy@garabatos.me}
+role :app, %w{deploy@ftp.garabatos.me}
+role :web, %w{deploy@ftp.garabatos.me}
+role :db,  %w{deploy@ftp.garabatos.me}
 
 server 'deploy@ftp.garabatos.me', user: 'deploy', roles: %w{web app}, my_property: :my_value
